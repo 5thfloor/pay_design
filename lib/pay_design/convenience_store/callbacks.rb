1ce31d0efadc87b6fea3645064a622b833e0a069
tnzk <tnzk@tnzk.org>
2016-06-29 14:47:00 +0900

    Impls and specs for callbacks

--- lib/pay_design/convenience_store/callbacks.rb
+++ lib/pay_design/convenience_store/callbacks.rb
@@ -1,10 +1,42 @@
 module PayDesign
   module ConvenienceStore
     module Callbacks
+      # Whilst they are named "Callbacks", they will just format parameters.
+      # How actual HTTP-level behaves depends on what the library user is.
+
       def self.payment_registered(params)
         {
           deal_id: params["SID"],
           remarks: PayDesign.encode_to_utf8(params["FUKA"]),
+        }
+      end
+
+      def self.payment_cancelled
+        # TODO: Nothing to do.
+      end
+
+      def self.payment_completed(params)
+        datetime_string = params["DATE"] + " " + params["TIME"]
+        {
+          notification_id: params["SEQ"],
+          paid_at:         DateTime.parse(datetime_string),
+          ip_code:         params["IP"],
+          deal_id:         params["SID"],
+          amount:          params["KINGAKU"],
+          store_type:      params["CVS"],
+          store_code:      params["SCODE"],
+          remarks:         PayDesign.encode_to_utf8(params["FUKA"]),
+        }
+      end
+
+      def self.payment_cancelled(params)
+        {
+          notification_id: params["SEQ"],
+          cancel_date:     Date.parse(params["DATE"]),
+          ip_code:         params["IP"],
+          deal_id:         params["SID"],
+          amount:          params["KINGAKU"],
+          remarks:         PayDesign.encode_to_utf8(params["FUKA"]),
         }
       end
     end
--- spec/pay_design/convenience_store/callbacks_spec.rb
+++ spec/pay_design/convenience_store/callbacks_spec.rb
@@ -9,4 +9,51 @@ describe PayDesign::ConvenienceStore::Callbacks do
       expect(registered_payment[:remarks]).to eq("深谷")
     end
   end
+
+  describe '.payment_completed' do
+    let (:params) {
+      {
+        "SEQ"     => "123456789",
+        "DATE"    => "20151211",
+        "TIME"    => "183059",
+        "IP"      => "D012345678",
+        "SID"     => "0123456789ABCDEF",
+        "KINGAKU" => "20000",
+        "CVS"     => "loppi",
+        "SCODE"   => "\n",
+        "FUKA"    => "深谷".encode(Encoding::SHIFT_JIS)
+      }
+    }
+    it 'maps their parametes to a hash with more meaningful fields' do
+      completed_payment = PayDesign::ConvenienceStore::Callbacks.payment_completed(params)
+      expect(completed_payment[:deal_id]).to eq("0123456789ABCDEF")
+      expect(completed_payment[:remarks]).to eq("深谷")
+    end
+    it 'maps its date and time fields to single datetime field' do
+      completed_payment = PayDesign::ConvenienceStore::Callbacks.payment_completed(params)
+      expect(completed_payment[:paid_at].class).to eq(DateTime)
+    end
+  end
+
+  describe '.payment_cancelled' do
+    let (:params) {
+      {
+        "SEQ"     => "123456789",
+        "DATE"    => "20151211",
+        "IP"      => "D012345678",
+        "SID"     => "0123456789ABCDEF",
+        "KINGAKU" => "20000",
+        "FUKA"    => "深谷".encode(Encoding::SHIFT_JIS)
+      }
+    }
+    it 'maps their parametes to a hash with more meaningful fields' do
+      completed_payment = PayDesign::ConvenienceStore::Callbacks.payment_cancelled(params)
+      expect(completed_payment[:deal_id]).to eq("0123456789ABCDEF")
+      expect(completed_payment[:remarks]).to eq("深谷")
+    end
+    it 'maps its date and time fields to single datetime field' do
+      completed_payment = PayDesign::ConvenienceStore::Callbacks.payment_cancelled(params)
+      expect(completed_payment[:cancel_date].class).to eq(Date)
+    end
+  end
 end
